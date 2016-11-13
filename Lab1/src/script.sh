aaf007bac74671c8ce4d8977e390663783807c59
rendanim <mbuvha.rendani@gmail.com>
2016-11-13 12:09:56 +0100

    Completed Second Part

--- Lab1/src/script.sh
+++ Lab1/src/script.sh
@@ -11,3 +11,14 @@ echo "------Finding the Maximum Length----------" >> ../results/results.txt
 cut -f 7  ../data/gpcr.tab | sort -g -r | head -1 >> ../results/results.txt
 echo "-------Unique Species---------------------" >> ../results/results.txt
 tail -n +2 ../data/gpcr.tab | cut -f 6 |sort | uniq | wc -l >> ../results/results.txt
+echo "-------------DNA-Alignment------------------">>../results/results.txt
+echo "------------instaling-----------------------">>../results/results.txt
+chmod +x ../bin/muscle
+echo "------------Aligning------------------------">>../results/results.txt
+for file in ../data/testatin/*;do
+     size=${#file}
+     l=$(($size-20))
+     ofile=${file:17:l}
+     echo $ofile >>../results/results.txt
+    ../bin/muscle -in $file -out ../results/$ofile.afa
+done
