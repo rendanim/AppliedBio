971c9f2d13ec1eb1d9960f3b56a80ea5b76ddbd8
rendanim <mbuvha.rendani@gmail.com>
2016-11-14 16:53:35 +0100

    reformat added

---
+++ Lab1/src/reformat.py
@@ -0,0 +1,34 @@
+n_max = 60
+
+def readstockhom():
+    filename = input('Which Sequence File?')
+
+    fp = open(filename)
+
+    for line in fp:
+        if(line[0] != '#' and line[0] != '//' and line[0] != '\n'):
+            linesplit=line.split()
+            if(len(linesplit)>1):
+                printName(linesplit[0])
+                printsequence(linesplit[1])
+            else:
+                printName(linesplit[0])
+
+def printName(name):
+    if (name !='//' and len(name)>0):
+        print('>'+name)
+
+def printsequence(seq):
+    olines = len(seq)//n_max
+    r=len(seq) % n_max
+    
+    for i in range(olines+1):
+        print(seq[(i*n_max):(((i+1)*n_max)-1)])
+    if(r>0):    
+        print(seq[(olines*n_max):r])
+
+def main():
+    readstockhom() 
+
+if __name__ == "__main__":
+    main()       
