09e9cf781508799ef0625671ec30a2d71f7c15ec
rendanim <mbuvha.rendani@gmail.com>
2016-11-15 11:54:57 +0100

    with comments

--- Lab1/src/reformat.py
+++ Lab1/src/reformat.py
@@ -6,10 +6,13 @@ def readstockhom():
     fp = open(filename)
 
     for line in fp:
+        #check for comments and end of file 
         if(line[0] != '#' and line[0] != '//' and line[0] != '\n'):
             linesplit=line.split()
             if(len(linesplit)>1):
+                #not just name
                 printName(linesplit[0])
+                #print the sequence
                 printsequence(linesplit[1])
             else:
                 printName(linesplit[0])
@@ -17,9 +20,13 @@ def readstockhom():
 def printName(name):
     if (name !='//' and len(name)>0):
         print('>'+name)
+    else:
+        print("Corner Case")    
 
 def printsequence(seq):
+    #find number of full lines
     olines = len(seq)//n_max
+    #find remaining lines
     r=len(seq) % n_max
     for i in range(olines):
         print(seq[(i*n_max):(((i+1)*n_max))])
