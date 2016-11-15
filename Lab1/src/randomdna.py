e9869176f9b320ee34841c8e150f9dcf39999ff9
rendanim <mbuvha.rendani@gmail.com>
2016-11-15 11:58:46 +0100

    comments

--- Lab1/src/randomdna.py
+++ Lab1/src/randomdna.py
@@ -1,12 +1,13 @@
 import random
-
+#Dna bases
 dna = ["A","G","C","T"]
 length=input("Length:")
 l=int(length)
 random_sequence=''
 
 print(">myrandomsequence")
- 
+''' append the the sequence by randomly choosing a base
+''' 
 for i in range(0,l):
     random_sequence+=random.choice(dna)
 
