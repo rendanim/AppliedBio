75d7d65f1587668782616043e1ec0193aa1d7ebc
rendanim <mbuvha.rendani@gmail.com>
2016-11-15 10:11:31 +0100

    comments and bug fix

--- Lab1/src/script.sh
+++ Lab1/src/script.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-echo "-------------Running WordCount------------" > ../results/results.txt
-tail -n +2 ../data/gpcr.tab |wc -l >> ../results/results.txt
-echo "---------Using Grep for Human counts------" >> ../results/results.txt
-grep -c "HUMAN" ../data/gpcr.tab >> ../results/results.txt
-echo "--------Homo sapiens----------------------" >> ../results/results.txt
-grep -c "Homo sapiens" ../data/gpcr.tab  >> ../results/results.txt
-echo "-----Finding the Minimum Length-----------" >> ../results/results.txt
-tail -n +2 ../data/gpcr.tab | cut -f 7 | sort -g|head -n +1 >> ../results/results.txt
-echo "------Finding the Maximum Length----------" >> ../results/results.txt
-cut -f 7  ../data/gpcr.tab | sort -g -r | head -1 >> ../results/results.txt
-echo "-------Unique Species---------------------" >> ../results/results.txt
-tail -n +2 ../data/gpcr.tab | cut -f 6 |sort | uniq | wc -l >> ../results/results.txt
-echo "-------------DNA-Alignment------------------">>../results/results.txt
-echo "------------instaling-----------------------">>../results/results.txt
+echo "-------------Running WordCount------------"
+tail -n +1 ../data/gpcr.tab |wc -l
+#tail - n location in terms of number of lines '+' from begining
+#wc -l return only number of lines
+
+echo "---------Using Grep for Human counts------"
+grep -c "HUMAN" ../data/gpcr.tab
+#grep -c print the number of lines 
+echo "-----Finding the Minimum Length-----------"
+tail -n +2 ../data/gpcr.tab | cut -f 7 | sort -g|head -n +1
+#cut f  - specifies the field f
+#sort g - specifies numeric sort -r reverse
+echo "------Finding the Maximum Length----------"
+cut -f 7  ../data/gpcr.tab | sort -g -r | head -n 1  
+echo "-------Unique Species---------------------" 
+tail -n +2 ../data/gpcr.tab | cut -f 6 |sort | uniq | wc -l
+echo "-------------DNA-Alignment------------------"
+echo "------------Changing Permisions-----------------------"
 chmod +x ../bin/muscle
-echo "------------Aligning------------------------">>../results/results.txt
+echo "------------Aligning------------------------"
 for file in ../data/testatin/*;do
-     size=${#file}
+     size=${#file} 
      l=$(($size-20))
      ofile=${file:17:l}
-     echo $ofile >>../results/results.txt
+     echo $ofile proccesed
     ../bin/muscle -in $file -out ../results/$ofile.afa
 done
