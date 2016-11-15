ce3f7b60d78d38f59fbdc0aa90958da398b6b35c
sud0301 <sudhanshu0301@gmail.com>
2016-11-15 11:11:43 +0100

    test files added

---
+++ Lab1/src/test.py
@@ -0,0 +1,77 @@
+#import pdb 
+import sys
+
+dnaDictionary = {}
+
+
+def loadSTable():
+	dnafile = open("stddna.txt")
+	for line in dnafile:
+		llist = line.strip().split(',')
+		l=len(llist)
+		for key in llist[0:l-1]:
+			dnaDictionary[key]=llist[l-1]
+	#print(dnaDictionary['TAA'])
+
+def readInput():
+	dnaSeq = ""
+	#fileInput = input("Write input file name here:")
+	#file = open(fileInput)
+	file = open("an_exon.fa.txt")
+	#file = open("translationtest.dna")
+	DNASeq = ""
+	lines = file.readlines()
+	last = lines[-1]
+	i=0
+	names=list()	
+	for line in lines:
+		if ">" in line:
+			names.append(line.split()[0])	
+			if i!=0:
+				print(names[i-1])
+			translateDNA(DNASeq)
+			DNASeq=""
+			i+=1
+		else:
+			DNASeq+=line.replace('\n','')
+			if line is last:	
+			#	print (DNASeq)
+				print(names[i-1])
+				translateDNA(DNASeq)	
+		
+	#print(names)
+def translateDNA(seq):
+	protSeq = ""
+	flag=0	
+	i=0;
+	if len(seq)>0:
+		while i < len(seq)-2:
+			value = mapping(seq[i:(i+3)]) 
+			#pdb.set_trace()
+			if value == 'STOP':
+				i = i+1
+				protSeq+='#'
+				continue
+			else:
+				i = i+3
+				protSeq += value
+		printSeq (protSeq)	
+
+	
+def mapping(codon):
+	if codon.upper() in dnaDictionary:
+		return dnaDictionary[codon.upper()]
+	else:
+		return 'X'
+	
+def printSeq(seq):
+	listSeq = seq.split('#')
+	print (max(listSeq, key=len))
+
+def main():
+	loadSTable()
+	readInput()
+
+
+if __name__ == "__main__":
+    main()
