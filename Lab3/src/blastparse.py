7d1849545992cd5f453d25e032117dcf9c9d7ac6
rendanim <mbuvha.rendani@gmail.com>
2016-12-05 15:38:28 +0100

    Added parsing blast

--- Lab3/src/blastparse.py
+++ Lab3/src/blastparse.py
@@ -3,6 +3,7 @@ from Bio import SeqIO
 import sys
 from Bio.Blast import NCBIXML
 import re
+from operator import itemgetter
 
 threshold = 1e20
 pattern=sys.argv[1]
@@ -13,10 +14,29 @@ blast_records = NCBIXML.parse(result_handle)
 
 #print(len(blast_records))
 #print(blast_records[0].alignments)
-
+output=list()
 for record in blast_records:
         for alignment in record.alignments:
             # hsp = high scoring pair
             for hsp in alignment.hsps:
-                print(hsp.expect)
-                
+                if (hsp.expect < threshold and re.search(pattern, alignment.title)):
+                    #print(record.query,end='\t')
+                    assession_sub = alignment.title.split('lcl|')[1].split(' ')[0]
+                    #print(assession_sub,end='\t')
+                    #print(hsp.score,end='\t')
+                    #print(hsp.expect)
+                    output.append([record.query, assession_sub,hsp.score,hsp.expect])     
+
+
+sort_out=sorted(output,key=itemgetter(1, 3))
+#print(sort_out)
+#sort remove higher e-values
+for i in range(len(sort_out)):
+    if(i>0 and sort_out[i][1]==sort_out[i-1][1]):
+        continue
+    else:           
+        print(sort_out[i][0],end='\t')
+        print(sort_out[i][1],end='\t')
+        print(sort_out[i][2],end='\t')
+        print(sort_out[i][3])
+                    
